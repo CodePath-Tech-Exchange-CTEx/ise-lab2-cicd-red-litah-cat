6edd0c8003656c9a8efbe3dba9d19cab20f64886
Jonathan King <jonathan.king1@bison.howard.edu>
2026-02-24 02:56:48 +0000

    add component functionality to app

--- app.py
+++ app.py
@@ -8,6 +8,7 @@
 import streamlit as st
 from modules import display_my_custom_component, display_post, display_genai_advice, display_activity_summary, display_recent_workouts
 from data_fetcher import get_user_posts, get_genai_advice, get_user_profile, get_user_sensor_data, get_user_workouts
+import datetime
 
 userId = 'user1'
 
