fd1a17b359a6847cde2c9d56427b389dfd5abfc2
God'swill ogbonna <godswillogbonna113@gmail.com>
2026-02-24 01:31:46 +0000

    not neede now

--- modules_test.py
+++ modules_test.py
@@ -22,30 +22,9 @@ class TestDisplayPost(unittest.TestCase):
 
 class TestDisplayActivitySummary(unittest.TestCase):
     """Tests the display_activity_summary function."""
-
-    def _call(self, workouts_list):
-        """Helper: calls display_activity_summary and returns the data dict
-        passed to create_component."""
-        with patch('modules.create_component') as mock_cc:
-            display_activity_summary(workouts_list)
-            args, _ = mock_cc.call_args
-            return args  # (data_dict, html_file_name)
-
-    def test_correct_html_file(self):
-        """create_component is called with the 'activity_summary' template."""
-        with patch('modules.create_component') as mock_cc:
-            display_activity_summary([])
-            _, html_file = mock_cc.call_args[0]
-        self.assertEqual(html_file, 'activity_summary')
-
-    def test_empty_list_totals_are_zero(self):
-        """An empty list produces zero for all three aggregate stats."""
-        data, _ = self._call([])
-        self.assertEqual(data['TOTAL_WORKOUTS'], 0)
-        self.assertEqual(data['TOTAL_CALORIES'], 0)
-        self.assertEqual(data['TOTAL_DURATION'], 0)
-
-
+    def test_foo(self):
+        """Tests foo."""
+    pass
 
 class TestDisplayGenAiAdvice(unittest.TestCase):
     """Tests the display_genai_advice function."""
