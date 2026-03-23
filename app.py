69697db0a658441275accb78a2b6233a02deb4cb
Toyosi <tsajiboye11@[my.fisk.edu]>
2026-03-23 04:35:16 +0000

    Implement Community Page

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ import streamlit as st
 from modules import display_my_custom_component, display_post, display_genai_advice, display_activity_summary, display_recent_workouts
 from data_fetcher import get_user_posts, get_genai_advice, get_user_profile, get_user_sensor_data, get_user_workouts
 from activity_page import display_activity_page
+from community_page import display_community_page
 import datetime
  
 userId = 'user1'
  
 def display_app_page():
-    """Displays the home page of the app."""
+    st.header("Posts")
+
+    #Displays the home page of the app.
  
     # --- Post logic ---
     user_profile = get_user_profile(userId)
@@ -61,7 +64,7 @@ if __name__ == '__main__':
         display_app_page()
     
     with community_tab:
-        st.info("Coming soon!")
+        display_community_page()
  
     with activity_tab:
         display_activity_page()
