48b491a0295ceeea149eff764b23a7954d3e61dc
Jonathan King <jonathan.king1@bison.howard.edu>
2026-04-23 21:20:23 +0000

    changed community page to last 10 posts in db

--- community_page.py
+++ community_page.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from modules import display_post, display_genai_advice
-from data_fetcher import get_user_posts, get_user_friends, get_genai_advice
+from data_fetcher import get_user_posts, get_user_friends, get_genai_advice, get_last_x_posts
 
 userId = 'user2'
 
-def display_community_page():
+def display_community_page(userId):
     """displays the community page including:
     First 10 posts from a user’s friends ordered by timestamp
     One piece of GenAI advice and encouragement
@@ -12,12 +12,7 @@ def display_community_page():
     st.header("POSTS")
 
     #POSTS
-    user_friends = get_user_friends(userId)
-    posts = []
-    for friend in user_friends:
-        posts.extend(get_user_posts(friend['user_id']))
-    posts.sort(key=lambda x: x['timestamp'], reverse=True)
-    posts = posts[:10]
+    posts = get_last_x_posts(10)
 
     #GENAI
     genai_advice = get_genai_advice(userId)
@@ -26,7 +21,7 @@ def display_community_page():
     #st.divider()
     #st.subheader("POSTS")
     for post in posts:
-        display_post(post['user_id'], None, post['timestamp'], post['content'], post['image'])
+        display_post(post['username'], None, post['timestamp'], post['content'], post['image'])
 
     st.divider()
     st.subheader("GENAI ADVICE")
